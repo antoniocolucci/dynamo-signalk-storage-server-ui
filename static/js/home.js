85920dec08b97b5de44818efaeb8a77e67eef4b9
antoniocolucci <antonio.colucci@intecs.it>
2023-07-17 11:30:34 +0200

    Add timeline

--- static/js/home.js
+++ static/js/home.js
@@ -1,29 +1,6 @@
-var map;
-var timeline;
-
 jQuery(function () {
-    initMap();
     updateDateTime();
 });
-
-// Inizializza la mappa
-function initMap() {
-    const map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM() // Utilizza OpenStreetMap come layer di base
-            })
-        ],
-        view: new ol.View({
-            center: [0, 0],
-            zoom: 2
-        }),
-        //timeline: new ol.Timeline({map: map, timeline: "#timeline", date_key: "when", format: ol.Format.GeoJSONTimeline})
-    });
-
-}
-
 
 function updateDateTime() {
     var now = new Date();
@@ -33,3 +10,93 @@ function updateDateTime() {
 
 // Aggiorna l'orario ogni secondo
 setInterval(updateDateTime, 1000);
+
+// The map
+var map = new ol.Map ({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM() // Utilizza OpenStreetMap come layer di base
+        })
+    ],
+    view: new ol.View({
+        center: [0, 0],
+        zoom: 2
+    }),
+});
+
+// Create Timeline control
+
+var tline = new ol.control.Timeline({
+    className: 'ol-pointer',
+    features: [{
+        text: 2023,
+        date: new Date('2023/01/01'),
+        endDate: new Date('2023/12/31')
+    }],
+    graduation: 'day', // 'month'
+    minDate: new Date('2023/01/01'),
+    maxDate: new Date('2023/12/31'),
+    getHTML: function(f){ return 2023; },
+    getFeatureDate: function(f){ return f.date; },
+    endFeatureDate: function(f) { return f.endDate }
+});
+map.addControl (tline);
+
+// Set the date when ready
+setTimeout(function(){ tline.setDate('2023'); });
+tline.addButton ({
+    className: "go",
+    title: "GO!",
+    handleClick: function() {
+        go();
+    }
+});
+
+// Show features on scroll
+/*tline.on('scroll', function(e){
+    var d = tline.roundDate(e.date, 'day')
+    $('.dateStart').text(d.toLocaleDateString(undefined, {year: 'numeric', month: 'short', day: 'numeric'}));
+    // Filter features visibility
+    vectorSource.getFeatures().forEach(function(f) {
+        var dt = f.get('time') - e.date;
+        if (Math.abs(dt) > 1000*3600*12) {
+            f.setStyle([]);
+        } else {
+            f.setStyle();
+        }
+    });
+});*/
+
+// Run on the timeline
+var running = false;
+var start = new Date('2022');
+var end = new Date('2023');
+function go(next) {
+    var date = tline.getDate();
+    if (running) clearTimeout(running);
+    if (!next) {
+        // stop
+        if (date>start && date<end && running) {
+            running = false;
+            tline.element.classList.remove('running');
+            return;
+        }
+        if (date > end) {
+            date = start;
+        }
+    }
+    if (date > end) {
+        tline.element.classList.remove('running');
+        return;
+    }
+    if (date < start) {
+        date = start;
+    }
+    // 1 day
+    date = new Date(date.getTime() + 24*60*60*1000);
+    tline.setDate(date, { anim:false });
+    // next
+    tline.element.classList.add('running');
+    running = setTimeout(function() { go(true); }, 100);
+}
